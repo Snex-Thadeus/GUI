d04e8668cc83870f3643449b9f8e98cd895a99c8
Snex Thadeus <lwande94@gmail.com>
2020-10-14 00:20:43 +0300

    Python GUI using Tkinter basics

---
+++ images.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Snex-Tech Systems")
+#root.iconbitmap('C:\Users\HOMELUX\PycharmProjects\GUI\codemy.ico')
+
+my_img = ImageTk.PhotoImage(Image.open("me.jpg"))
+my_label = Label(image=my_img)
+my_label.pack()
+
+
+button_quit = Button(root, text="Exit Program", command=root.quit)
+button_quit.pack()
+
+root.mainloop()
