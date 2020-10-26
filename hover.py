21e27909be1fde001e9d5cf9cff5ceccc8204446
Snex-Thadeus <lwande94@gmail.com>
2020-10-27 01:15:38 +0300

    Mouse on Hover Image Animation

---
+++ hover.py
@@ -0,0 +1,35 @@
+from tkinter import *
+#from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Snex-Tech Systems")
+root.geometry("800x500")
+root.iconbitmap('snex.ico')
+
+
+def change(e):
+    my_pic = PhotoImage(file="images/cap.jpg")
+    my_label.config(image=my_pic)
+    my_label.image = my_pic
+
+def change_back(e):
+    my_pic = PhotoImage(file="images/cy.jpg")
+    my_label.config(image=my_pic)
+    my_label.image = my_pic
+
+def do_something():
+    label2 = Label(root, text="You clicked the button!")
+    label2.pack()
+
+my_pic = PhotoImage(file="images/me.jpg")
+my_label = Button(root, image=my_pic, command=do_something)
+my_label.pack(pady=20)
+
+def exit(e):
+	root.quit()
+
+my_label.bind("<Enter>", change)
+my_label.bind("<Leave>", change_back)
+my_label.bind("<Return>", exit)
+
+root.mainloop()
