2bbc4680e1f3aee9808dd32f3031a6cd51275451
Snex-Thadeus <lwande94@gmail.com>
2020-10-27 00:52:12 +0300

    Creating a Date Picker Calendar

---
+++ my_cal.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkcalendar import *
+
+root = Tk()
+root.title('Snex-Tech')
+root.iconbitmap('snex.ico')
+root.geometry("600x400")
+
+
+cal = Calendar(root, selectmode="day", year=2020, month=10, day=20)
+cal.pack()
+
+def grab_date():
+	my_label.config(text="Today's Date Is " + cal.get_date())
+
+my_button = Button(root, text="Get Date", command=grab_date)
+my_button.pack(pady=20)
+
+my_label = Label(root, text="")
+my_label.pack(pady=20)
+
+
+root.mainloop()
