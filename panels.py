86a14c405b0dbf2d9742d8a71b59d933d41c42fd
Snex-Thadeus <lwande94@gmail.com>
2020-10-26 17:48:18 +0300

    Paned Windows in tkinter

---
+++ panels.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+root = Tk()
+root.title("Snex-Tech!")
+root.geometry("400x400")
+root.iconbitmap('snex.ico')
+
+# Panels
+panel_1 = PanedWindow(bd=4, relief="raised", bg="red")
+panel_1.pack(fill=BOTH, expand=1)
+
+left_label = Label(panel_1, text="Left Panel")
+panel_1.add(left_label)
+
+# create second panel
+panel_2 = PanedWindow(panel_1, orient=HORIZONTAL, bd=4, relief="raised", bg="blue")
+panel_1.add(panel_2)
+
+top = Label(panel_2, text="Top Panel")
+panel_2.add(top)
+
+bottom = Label(panel_2, text="Bottom Panel")
+panel_2.add(bottom)
+
+
+root.mainloop()
